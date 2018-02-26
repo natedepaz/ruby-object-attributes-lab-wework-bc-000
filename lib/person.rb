234b19ce84cd37103e625f4edbb918bccf4d7968
Nathan de Paz <nathan.depaz@wework.com>
2018-02-26 09:16:27 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,2 +1,16 @@
 class Person
-  
+  def name 
+  person_name = @name 
+ end
+ 
+ def name=(new_name)
+   @name = new_name
+ end
+ 
+ def breed 
+   dog_breed = @breed
+ end
+ 
+ def breed=(new_breed)
+   @breed = new_breed
+ end 
