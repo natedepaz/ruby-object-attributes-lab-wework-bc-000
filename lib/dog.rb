7457b6eb92151c2c7be89e8040f106a61c946012
Nathan de Paz <nathan.depaz@wework.com>
2018-02-26 09:19:17 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+ def name 
+  dog_name = @name 
+ end
+ 
+ def name=(new_name)
+   @name = new_name
+ end
+ 
+ def breed 
+   dog_breed = @breed
+ end
+ 
+ def breed=(new_breed)
+   @breed = new_breed
+ end 
+end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name 
+  person_name = @name 
+ end
+ 
+ def name=(new_name)
+   @name = new_name
+ end
+ 
+ def job 
+   person_job = @job
+ end
+ 
+ def job= (new_job)
+   @job = new_job
+ end
+end
